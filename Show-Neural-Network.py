389c32092620d5ae5a338e0cb0504ff8cd3948d8
Conor O'Mahony <36863560+conoromahony@users.noreply.github.com>
2024-03-22 13:51:19 -0400

    Got Previous and Next buttons working.

--- Show-Neural-Network.py
+++ Show-Neural-Network.py
@@ -23,7 +23,7 @@
 #  - Switch it so we write to a TempFiles directory, and then ovwerwrite the contents of the directory on the next run.
 #  - Is it possible to make this more efficient: read JSON --> create output_str --> create SVG
 
-from flask import Flask, render_template, redirect, url_for, make_response
+from flask import Flask, render_template, flash
 import numpy as np
 import json
 
@@ -95,17 +95,17 @@ def hello():
 @app.route('/next')
 def next_iteration():
     # TODO: Is it a problem that I am not closing the file, before opening another?
-    # The following line tells this module to use the global value of file_name_number in this function.
+    # The following lines tells this module to use the global values of variables in this function.
     global file_name_number
-    # TODO: Need to check haven't gone past last iteration!!!
-    file_name_number = str(int(file_name_number) + 1)
+    global num_iterations
+    if int(file_name_number) < int(num_iterations) - 1:
+        file_name_number = str(int(file_name_number) + 1)
+    else:
+        file_name_number = str(int(num_iterations) - 1)
     file_object = open(directory_name + file_name_base + file_name_number)
     working_data = json.load(file_object)
-    meta_data = working_data["metadata"]
     nodes_data = working_data["nodes"]
     connections_data = working_data["connections"]
-    iteration_number = meta_data[0]["iteration_number"]
-    direction = meta_data[0]["direction"]
     output_str = "var newGraph = { 'metadata': [], 'nodes': [], 'connections':[] }; \n"
     for item in working_data["metadata"][0].keys():
         new_str = "var " + str(item) + " = '" + str(working_data["metadata"][0][item]) + "'; \n"
@@ -124,15 +124,14 @@ def previous_iteration():
     # TODO: Is it a problem that I am not closing the file, before opening another?
     # The following line tells this module to use the global value of file_name_number in this function.
     global file_name_number
-    # TODO: Need to check haven't gone before first iteration!!!
-    file_name_number = str(int(file_name_number) - 1)
+    if int(file_name_number) > 0:
+        file_name_number = str(int(file_name_number) - 1)
+    else:
+        file_name_number = "0"
     file_object = open(directory_name + file_name_base + file_name_number)
     working_data = json.load(file_object)
-    meta_data = working_data["metadata"]
     nodes_data = working_data["nodes"]
     connections_data = working_data["connections"]
-    iteration_number = meta_data[0]["iteration_number"]
-    direction = meta_data[0]["direction"]
     output_str = "var newGraph = { 'metadata': [], 'nodes': [], 'connections':[] }; \n"
     for item in working_data["metadata"][0].keys():
         new_str = "var " + str(item) + " = '" + str(working_data["metadata"][0][item]) + "'; \n"
