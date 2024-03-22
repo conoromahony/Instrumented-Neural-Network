27498e7e44c2e56aba3f8ecc94bc0f18381c6eb6
Conor O'Mahony <36863560+conoromahony@users.noreply.github.com>
2024-03-22 11:51:40 -0400

    Complete formatting updates; start working on buttons to move through training

--- Show-Neural-Network.py
+++ Show-Neural-Network.py
@@ -23,14 +23,15 @@
 #  - Switch it so we write to a TempFiles directory, and then ovwerwrite the contents of the directory on the next run.
 #  - Is it possible to make this more efficient: read JSON --> create output_str --> create SVG
 
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, redirect, url_for, make_response
 import numpy as np
 import json
 
 
 directory_name = "Neural-Network-Parameters-20240124-1941/"
-file_name = "working-data-0"
-file_object = open(directory_name + file_name)
+file_name_base = "working-data-"
+file_name_number = "0"
+file_object = open(directory_name + file_name_base + file_name_number)
 working_data = json.load(file_object)
 meta_data = working_data["metadata"]
 nodes_data = working_data["nodes"]
@@ -91,8 +92,67 @@ def hello():
 
     return render_template("index.html", network_graph=output_str)
 
+@app.route('/next')
+def next_iteration():
+    # TODO: Is it a problem that I am not closing the file, before opening another?
+    # The following line tells this module to use the global value of file_name_number in this function.
+    global file_name_number
+    # TODO: Need to check haven't gone past last iteration!!!
+    file_name_number = str(int(file_name_number) + 1)
+    file_object = open(directory_name + file_name_base + file_name_number)
+    working_data = json.load(file_object)
+    meta_data = working_data["metadata"]
+    nodes_data = working_data["nodes"]
+    connections_data = working_data["connections"]
+    iteration_number = meta_data[0]["iteration_number"]
+    direction = meta_data[0]["direction"]
+    output_str = "var newGraph = { 'metadata': [], 'nodes': [], 'connections':[] }; \n"
+    for item in working_data["metadata"][0].keys():
+        new_str = "var " + str(item) + " = '" + str(working_data["metadata"][0][item]) + "'; \n"
+        output_str += new_str
+    for node in nodes_data:
+        new_str = "var tempNode = { 'id': " + str(node["id"]) + ", 'layer': " + str(node["layer"]) + ", 'node': " + str(node["node"]) + ", 'bias': " + str(node["bias"]) + "}; newGraph.nodes.push(tempNode); \n"
+        output_str += new_str
+    for link in connections_data:
+        new_str = "var tempLink = { 'source': " + str(link["source"]) + ", 'target': " + str(link["target"]) + ", 'weight': " + str(link["weight"]) + "}; newGraph.connections.push(tempLink); \n"
+        output_str += new_str
+    return render_template("index.html", network_graph=output_str)
 
-# *** CURRENTLY: adding incoming and outgoing lines/links to close-up view (in layout.html)
-# *** NEXT: finish adding lines/links (have to do for hidden layer and output layer)
-# *** THEN: figure out how to center the contents of the columns
-# *** ALSO: color the selected node
+
+@app.route('/previous')
+def previous_iteration():
+    # TODO: Is it a problem that I am not closing the file, before opening another?
+    # The following line tells this module to use the global value of file_name_number in this function.
+    global file_name_number
+    # TODO: Need to check haven't gone before first iteration!!!
+    file_name_number = str(int(file_name_number) - 1)
+    file_object = open(directory_name + file_name_base + file_name_number)
+    working_data = json.load(file_object)
+    meta_data = working_data["metadata"]
+    nodes_data = working_data["nodes"]
+    connections_data = working_data["connections"]
+    iteration_number = meta_data[0]["iteration_number"]
+    direction = meta_data[0]["direction"]
+    output_str = "var newGraph = { 'metadata': [], 'nodes': [], 'connections':[] }; \n"
+    for item in working_data["metadata"][0].keys():
+        new_str = "var " + str(item) + " = '" + str(working_data["metadata"][0][item]) + "'; \n"
+        output_str += new_str
+    for node in nodes_data:
+        new_str = "var tempNode = { 'id': " + str(node["id"]) + ", 'layer': " + str(node["layer"]) + ", 'node': " + str(node["node"]) + ", 'bias': " + str(node["bias"]) + "}; newGraph.nodes.push(tempNode); \n"
+        output_str += new_str
+    for link in connections_data:
+        new_str = "var tempLink = { 'source': " + str(link["source"]) + ", 'target': " + str(link["target"]) + ", 'weight': " + str(link["weight"]) + "}; newGraph.connections.push(tempLink); \n"
+        output_str += new_str
+    return render_template("index.html", network_graph=output_str)
+
+
+# *** CURRENTLY: adding buttons to step through training
+# *** Add checks for iterating too far eithger way
+# *** Add button to go to start or end
+# *** Add button to play / pause through iterations
+# *** Maintain node close-up view when going to next or previous
+
+# *** Maybe generate mathplotlib images upon training runs, store them in the folder, and display them in the UI?
+# ***  Training error rate
+# ***  Validation error rate
+# ***  Test error rate
